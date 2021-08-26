bf129b6a904044b5725b7b9ec94a1db84bfb944b
Hector Menduiña <hector@hectormhermelo.com>
2021-08-26 12:45:39 +0200

    Course: Touring Vue Router - 8. Flash Messages (notify)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div id="flashMessage" v-if="GStore.flashMessage">
+    {{ GStore.flashMessage }}
+  </div>
   <div id="nav">
     <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
@@ -6,7 +9,27 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  inject: ['GStore']
+}
+</script>
+
 <style lang="scss">
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  background: yellow;
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -7,9 +7,18 @@
 export default {
   name: 'EventRegister',
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // call to API
+
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+
       this.$router.push({
         name: 'EventDetails',
         params: { id: this.event.id }
