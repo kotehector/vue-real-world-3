ce20f17476cde94e0b79d979308d12585e5ab8c7
Hector Menduiña <hector@hectormhermelo.com>
2021-08-26 12:01:49 +0200

    Course: Touring Vue Router - 5. Redirect & Alias - 6. Programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,20 @@
 <template>
   <p>Register event</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   name: 'EventRegister',
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // call to API
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
