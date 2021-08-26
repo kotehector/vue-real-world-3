183eca070c239a09ec2bcbab5942c752694c5d2a
Hector Menduiña <hector@hectormhermelo.com>
2021-08-26 12:32:18 +0200

    Course: Touring Vue Router - 7. Error Handling and 404s

---
+++ src/views/NetworkError.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Uh-oh!!</h1>
+  <h3>
+    it looks like you're experiencing some network issues, please take a breath
+    and <a href="#" @click="$router.go(-1)">click here</a>{{ resource }} to try
+    again.
+  </h3>
+</template>
+
+<script>
+export default {
+  name: 'NetworkError'
+}
+</script>
