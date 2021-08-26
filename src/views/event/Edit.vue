3dfc86186ea040c8b50ba50f14ba81046867581f
Hector Menduiña <hector@hectormhermelo.com>
2021-08-26 11:27:54 +0200

    Course: Touring Vue Router - 4. Nested Routes

---
+++ src/views/event/Edit.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Edit event</p>
+</template>
+
+<script>
+export default {
+  name: 'EventEdit',
+  props: ['event']
+}
+</script>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Register event</p>
+</template>
+
+<script>
+export default {
+  name: 'EventRegister',
+  props: ['event']
+}
+</script>
